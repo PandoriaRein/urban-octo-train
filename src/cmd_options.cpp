883ee49b2238e4fca255d85c25798192131b1d48
CaspianMaster <starwars_98@mail.ru>
2025-05-18 17:47:44 +0300

    Тесты, поправил pre-commit

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -6,33 +6,30 @@ namespace po = boost::program_options;
 
 namespace CryptoGuard {
 
-    ProgramOptions::ProgramOptions() : desc_("Allowed options") {
-        desc_.add_options()
-        ("help", "produce help message")
-        ("command", po::value<std::string>(), "command")
-        ("i",po::value<std::string>(), "input")
-        ("o",po::value<std::string>(), "output")
-        ("p",po::value<std::string>(), "password")
-        ;
-    }
+ProgramOptions::ProgramOptions() : desc_("Allowed options") {
+  desc_.add_options()("help", "produce help message")(
+      "command", po::value<std::string>(),
+      "command")("i", po::value<std::string>(),
+                 "input")("o", po::value<std::string>(),
+                          "output")("p", po::value<std::string>(), "password");
+}
 
-    ProgramOptions::~ProgramOptions() = default;
+ProgramOptions::~ProgramOptions() = default;
 
-    void ProgramOptions::Parse(int argc, char *argv[]) 
-    {
-        po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, desc_), vm);
+void ProgramOptions::Parse(int argc, char *argv[]) {
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc_), vm);
 
-        po::notify(vm);  
-        
-        if (vm.count("help")) {
-            std::cout << desc_ << "\n";
-            return;
-        }
-        if (vm.count("i")) {
-            std::cout << desc_ << "\n";
-            return;
-        }
-    }
+  po::notify(vm);
 
-}  // namespace CryptoGuard
+  if (vm.count("help")) {
+    std::cout << desc_ << "\n";
+    return;
+  }
+  if (vm.count("i")) {
+    std::cout << desc_ << "\n";
+    return;
+  }
+}
+
+} // namespace CryptoGuard
--- tests/cmd_options_test.cpp
+++ tests/cmd_options_test.cpp
@@ -1,46 +1,58 @@
 #include "cmd_options.h"
 #include <gtest/gtest.h>
-namespace ProgramOptions
-{
-    TEST(ProgramOptions, TestName){
+namespace ProgramOptions {
+TEST(ProgramOptions, TestName) {
 
-
-        EXPECT_NO_THROW({
-            CryptoGuard::ProgramOptions programOpt;
-            char *argv = "help 123";
-            programOpt.Parse(2,&argv);
-        });
-    }
-
-    TEST(ProgramOptions, TestName2){ 
-        
-        
-        EXPECT_NO_THROW({
-            CryptoGuard::ProgramOptions programOpt;
-            char *argv = "command 123";
-            programOpt.Parse(2,&argv);
-        });
-    }
-    
-    TEST(ProgramOptions, TestName3){ 
-        
-        
-        EXPECT_THROW({
-            CryptoGuard::ProgramOptions programOpt;
-            char *argv = "help 123";
-            programOpt.Parse(6,&argv);
-        },std::logic_error);
-    }
-
-    TEST(ProgramOptions, TestName4){ 
-        
-        
-        EXPECT_THROW({
-            CryptoGuard::ProgramOptions programOpt;
-            char *argv = "help 123";
-            programOpt.Parse(6,&argv);
-        },std::logic_error);
-    }
-
-
+  EXPECT_NO_THROW({
+    CryptoGuard::ProgramOptions programOpt;
+    char *argv = "help 123";
+    programOpt.Parse(2, &argv);
+  });
 }
+
+TEST(ProgramOptions, TestName2) {
+
+  EXPECT_NO_THROW({
+    CryptoGuard::ProgramOptions programOpt;
+    char *argv = "command 123";
+    programOpt.Parse(2, &argv);
+  });
+}
+
+TEST(ProgramOptions, TestName3) {
+
+  EXPECT_THROW(
+      {
+        CryptoGuard::ProgramOptions programOpt;
+        char *argv = "help 123";
+        programOpt.Parse(6, &argv);
+      },
+      std::logic_error);
+}
+
+TEST(ProgramOptions, TestName4) {
+
+  EXPECT_NO_THROW({
+    CryptoGuard::ProgramOptions programOpt;
+    char *argv = "command encode";
+    programOpt.Parse(2, &argv);
+  });
+}
+
+TEST(ProgramOptions, TestName5) {
+
+  EXPECT_NO_THROW({
+    CryptoGuard::ProgramOptions programOpt;
+    char *argv = "-i input.txt -o output.exe";
+    programOpt.Parse(4, &argv);
+  });
+}
+TEST(ProgramOptions, TestName6) {
+
+  EXPECT_NO_THROW({
+    CryptoGuard::ProgramOptions programOpt;
+    char *argv = "command encode -i input.txt -o output.exe";
+    programOpt.Parse(6, &argv);
+  });
+}
+} // namespace ProgramOptions

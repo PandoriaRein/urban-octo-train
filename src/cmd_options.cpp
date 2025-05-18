c9c3cc5ccabdb60bd817f2c8276ef3638d0b658a
CaspianMaster <starwars_98@mail.ru>
2025-05-18 12:41:10 +0300

    Второй тестовый коммит

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -6,6 +6,6 @@ ProgramOptions::ProgramOptions() : desc_("Allowed options") {}
 
 ProgramOptions::~ProgramOptions() = default;
 
-void ProgramOptions::Parse(int argc, char *argv[]) {}
+void ProgramOptions::Parse(int argc, char *argv[]) {int x =1;}
 
 }  // namespace CryptoGuard
